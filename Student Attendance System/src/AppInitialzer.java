80e4d2a0f249fb0bc82eb8bf29c8a7319c593fe7
Bhashitha Arambepola <42783927+BhashithaArambepola@users.noreply.github.com>
2022-03-11 12:46:39 +0530

    add SplashScreen

--- Student Attendance System/src/AppInitialzer.java
+++ Student Attendance System/src/AppInitialzer.java
@@ -1,5 +1,12 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class AppInitialzer extends Application {
 
@@ -8,7 +15,15 @@ public class AppInitialzer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("View/SplashScreen.fxml"));
+        Scene scene=new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+        scene.setFill(Color.TRANSPARENT);
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.sizeToScene();
 
     }
 }
---
+++ Student Attendance System/src/Controller/SplashScreenController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.scene.control.Label;
+
+public class SplashScreenController {
+    public Label lblStatus;
+    public void initialize(){
+
+    }
+
+}
