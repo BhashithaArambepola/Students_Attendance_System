e3d6c9130f4545f3ee101581a6879e25b1685191
Bhashitha Arambepola <42783927+BhashithaArambepola@users.noreply.github.com>
2022-03-11 13:15:47 +0530

    add login form

---
+++ Student Attendance System/src/Controller/LogInFormController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public Button btnSignIn;
+
+    public void btnSignIn_OnAction(ActionEvent actionEvent) {
+    }
+}
--- Student Attendance System/src/Controller/SplashScreenController.java
+++ Student Attendance System/src/Controller/SplashScreenController.java
@@ -1,10 +1,42 @@
 package Controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SplashScreenController {
     public Label lblStatus;
     public void initialize(){
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Platform.runLater(() -> {
+
+                try {
+                    Stage stage = new Stage();
+                    AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/LogInForm.fxml"));
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.sizeToScene();
+                    stage.setResizable(false);
+                    stage.centerOnScreen();
+                    stage.setTitle("Student Attendance: LogIn ");
+                    stage.show();
+                    ((Stage)lblStatus.getScene().getWindow()).close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }).start();
 
     }
 
