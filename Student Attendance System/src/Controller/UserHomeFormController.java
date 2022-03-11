41404888d59263f3d0c176c17a1abdb48c5aa603
Bhashitha Arambepola <42783927+BhashithaArambepola@users.noreply.github.com>
2022-03-11 13:50:08 +0530

    add user Home Form

---
+++ Student Attendance System/src/Controller/UserHomeFormController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class UserHomeFormController {
+    public Button btnRecordAttendance;
+    public Button btnViewReports;
+    public Button btnUserProfile;
+    public Button btnSignOut;
+    public Label lblGreeting;
+    public Label lblHover;
+
+    public void btnSignOut_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUserProfile_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewReports_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRecordAttendance_OnAction(ActionEvent actionEvent) {
+    }
+}
