04358586bb57e5be66b9eb00a2ffbb781d5af90e
Bhashitha Arambepola <42783927+BhashithaArambepola@users.noreply.github.com>
2022-03-11 14:13:16 +0530

    add Alert Form

---
+++ Student Attendance System/src/Controller/AlertForController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AlertForController {
+    public Label lblDescription;
+    public Label lblId;
+    public Label lblName;
+    public Label lblDate;
+    public Button btnProceed;
+    public Button btnCallPolice;
+
+    public void btnProceed_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCallPolice_OnAction(ActionEvent actionEvent) {
+    }
+}
