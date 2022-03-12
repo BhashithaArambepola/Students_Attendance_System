23aa2375f598a4599ff1a2852dcb858766d28f4b
Bhashitha Arambepola <42783927+BhashithaArambepola@users.noreply.github.com>
2022-03-12 21:38:37 +0530

    add DB form, implement splash screen

---
+++ Student Attendance System/src/Controller/ImportDBFormController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ImportDBFormController {
+    public RadioButton rdoRestore;
+    public ToggleGroup abc;
+    public TextField txtBrowse;
+    public Button btnBrowse;
+    public RadioButton rdoFirstTime;
+    public Button btnOK;
+
+    public void btnBrowse_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOK_OnAction(ActionEvent actionEvent) {
+    }
+}
--- Student Attendance System/src/Controller/SplashScreenController.java
+++ Student Attendance System/src/Controller/SplashScreenController.java
@@ -8,36 +8,62 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SplashScreenController {
     public Label lblStatus;
-    public void initialize(){
-        new Thread(() -> {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Platform.runLater(() -> {
-
-                try {
-                    Stage stage = new Stage();
-                    AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/UserHomeForm.fxml"));
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.sizeToScene();
-                    stage.setResizable(false);
-                    stage.centerOnScreen();
-                    stage.setTitle("Student Attendance: LogIn ");
-                    stage.show();
-                    ((Stage)lblStatus.getScene().getWindow()).close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }).start();
-
+    public void initialize() {
+        establishDBConnection();
     }
 
+private void establishDBConnection(){
+        lblStatus.setText("Establishing DataBase Connection...");
+        new Thread(() -> {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection connection = DriverManager.getConnection("\"jdbc:mysql://localhost:3306/dep8_student_attendance", "root", "78527852");
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                if (e.getSQLState().equals("42000")){
+                    Platform.runLater(this::loadImportDBForm);
+                }
+            }
+        });
+
 }
+
+private void loadImportDBForm(){
+//    new Thread(() -> {
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        Platform.runLater(() -> {
+
+            try {
+                Stage stage = new Stage();
+                AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/UserHomeForm.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.sizeToScene();
+                stage.setResizable(false);
+                stage.centerOnScreen();
+                stage.setTitle("Student Attendance: LogIn ");
+                stage.show();
+                ((Stage)lblStatus.getScene().getWindow()).close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//        });
+//    }).start();
+//
+}
+}
+
