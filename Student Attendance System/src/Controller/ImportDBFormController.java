084e50462f0924c2bc77cf6101db5279b89ba1b0
bhashitha Arambepola <barambepola@gmail.com>
2022-03-27 14:19:32 +0530

    finalize DB Form

--- Student Attendance System/src/Controller/ImportDBFormController.java
+++ Student Attendance System/src/Controller/ImportDBFormController.java
@@ -1,10 +1,15 @@
 package Controller;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class ImportDBFormController {
     public RadioButton rdoRestore;
@@ -13,10 +18,30 @@ public class ImportDBFormController {
     public Button btnBrowse;
     public RadioButton rdoFirstTime;
     public Button btnOK;
+    private SimpleObjectProperty<File> fileProperty;
 
+    public void initialize()
+    {
+        txtBrowse.setDisable(false);
+    }
+
+    public void initFileProperty(SimpleObjectProperty<File>fileProperty){
+        this.fileProperty=fileProperty;
+    }
     public void btnBrowse_OnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a Backup File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Backup file","*.dep8Backup"));
+        File file = fileChooser.showOpenDialog(btnOK.getScene().getWindow());
+        txtBrowse.setText(file!= null? file.getAbsolutePath():"");
+        fileProperty.setValue(file);
     }
 
     public void btnOK_OnAction(ActionEvent actionEvent) {
+        if (rdoFirstTime.isSelected()){
+            fileProperty.setValue(null);
+
+        }
+        ((Stage)(btnOK.getScene().getWindow())).close();
     }
 }
--- Student Attendance System/src/Controller/SplashScreenController.java
+++ Student Attendance System/src/Controller/SplashScreenController.java
@@ -1,12 +1,15 @@
 package Controller;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,12 +17,15 @@ import java.sql.SQLException;
 
 public class SplashScreenController {
     public Label lblStatus;
+    private File file;
+
     public void initialize() {
         establishDBConnection();
     }
 
-private void establishDBConnection(){
+    private void establishDBConnection() {
         lblStatus.setText("Establishing DataBase Connection...");
+
         new Thread(() -> {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
@@ -29,41 +35,35 @@ private void establishDBConnection(){
                 e.printStackTrace();
             } catch (SQLException e) {
                 e.printStackTrace();
-                if (e.getSQLState().equals("42000")){
+                if (e.getSQLState().equals("42000")) {
                     Platform.runLater(this::loadImportDBForm);
                 }
             }
         });
 
-}
+    }
 
-private void loadImportDBForm(){
-//    new Thread(() -> {
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        Platform.runLater(() -> {
+    private void loadImportDBForm() {
+        try {
 
-            try {
-                Stage stage = new Stage();
-                AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/UserHomeForm.fxml"));
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.sizeToScene();
-                stage.setResizable(false);
-                stage.centerOnScreen();
-                stage.setTitle("Student Attendance: LogIn ");
-                stage.show();
-                ((Stage)lblStatus.getScene().getWindow()).close();
+            SimpleObjectProperty<File>fileProperty=new SimpleObjectProperty<>();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//        });
-//    }).start();
-//
-}
+            Stage stage = new Stage();
+            AnchorPane root = FXMLLoader.load(this.getClass().getResource("/View/ImportDBForm.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.sizeToScene();
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.setTitle("Student Attendance: LogIn ");
+            stage.showAndWait();
+            stage.initOwner( lblStatus.getScene().getWindow());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
 
